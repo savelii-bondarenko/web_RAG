886e0a0a1591e4a7f42b60452452482f14d451c9
Savelii Bondarenko <savabondarenko2016@gmail.com>
2025-12-30 02:45:20 +0200

    some refactor

--- RAG_API/RAG_Graph.py
+++ RAG_API/RAG_Graph.py
@@ -1,5 +1,7 @@
 from utills import read_data, split_text, Embedder, create_vectorDB
 from langchain_core.documents import Document
+from numpy import ndarray
+from faiss import Index
 import logging
 
 logging.basicConfig(
@@ -9,17 +11,10 @@ logging.basicConfig(
 
 data = "1.docx"
 
-#read data
 extracted_text: str = read_data(data)
-
-#split text
 splitted_text: list[Document] = split_text(extracted_text)
-
-#create embeddings
-embeddings = Embedder().make_embeddings(splitted_text)
-
-#create vectorDB
-vectorDB = create_vectorDB(embeddings)
+embeddings: ndarray = Embedder().make_embeddings(splitted_text)
+vectorDB: Index = create_vectorDB(embeddings)
 
 
 
