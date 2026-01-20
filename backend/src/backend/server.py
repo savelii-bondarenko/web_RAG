1bf9ff4e6de99d0034cd6958df4cd1935f0e8bf1
savelii-bondarenko <savabondarenko2016@gmail.com>
2026-01-20 23:48:16 +0200

    add message history

--- backend/src/backend/server.py
+++ backend/src/backend/server.py
@@ -2,24 +2,34 @@ import os
 import tempfile
 import asyncio
 import uuid
+
+from typing import Literal
+
 from fastapi import FastAPI, UploadFile, File, HTTPException, Form
 from pydantic import BaseModel, ConfigDict
 
 from rag import prepare_rag_assets, RAGGraph
 app = FastAPI()
 
-rag_sessions: dict[str, RAGGraph] = {}
-
 class User(BaseModel):
+    message_history: list[str]
+    model_config = ConfigDict(extra='forbid')
+
+rag_sessions: dict[str, tuple[RAGGraph, User]] = {}
+
+class UserMessage(BaseModel):
     """
     Schema for a user chat message.
 
     Attributes:
-        message (str): The user's input message.
+        session_id (str): The user's session_id.
     """
     session_id: str
     message: str
     model_config = ConfigDict(extra='forbid')
+
+
+
 
 def generate_id() -> str:
     """Generate a unique ID for a user.
@@ -45,7 +55,7 @@ async def health_check():
 async def upload_file(
         session_id: str | None = Form(None),
         file: UploadFile = File(...)
-    ):
+):
     """
     Upload a file and initialize the RAG pipeline.
 
@@ -72,8 +82,9 @@ async def upload_file(
         tmp_path = tmp.name
 
     try:
-        new_rag_instance = await asyncio.to_thread(initialize_rag_sync, tmp_path)
-        rag_sessions[session_id] = new_rag_instance
+        new_rag_instance: RAGGraph = await asyncio.to_thread(initialize_rag_sync, tmp_path)
+        new_user = User(message_history=[])
+        rag_sessions[session_id] = (new_rag_instance, new_user)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"File upload error: {str(e)}")
     finally:
@@ -84,7 +95,7 @@ async def upload_file(
             "session_id": session_id}
 
 @app.post("/chat")
-async def send_message(user_data: User):
+async def send_message(user_data: UserMessage):
     """
     Send a user message to the RAG bot and return its response.
 
@@ -98,9 +109,13 @@ async def send_message(user_data: User):
     Raises:
         HTTPException: If no file has been uploaded before querying the bot.
     """
-    current_rag = rag_sessions.get(user_data.session_id)
+    current_rag: RAGGraph = rag_sessions[user_data.session_id][0]
+    current_user: User = rag_sessions[user_data.session_id][1]
 
     if current_rag is None:
         raise HTTPException(status_code=400, detail="Firstly upload file /upload")
-    result = await asyncio.to_thread(get_answer_sync, current_rag, user_data.message)
+    current_user.message_history.append(user_data.message)
+    message: str = "\n".join(current_user.message_history)
+    result = await asyncio.to_thread(get_answer_sync, current_rag, message)
+    current_user.message_history.append(result)
     return {"response": result}
