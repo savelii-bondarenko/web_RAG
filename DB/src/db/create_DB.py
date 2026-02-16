5ad75b9865197c7128b5aec8fd6d423c10d8b116
sawa <savabondarenko2016@gmail.com>
2026-02-16 19:35:19 +0200

    add create_DB func

--- DB/src/db/create_DB.py
+++ DB/src/db/create_DB.py
@@ -1,10 +1,26 @@
 import os
-
 import psycopg
+import logging
 
-conn_params = {
-    "host": "localhost",
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-}
+conn_params = f"host=localhost user=postgres password={os.getenv('POSTGRES_PASSWORD')}"
+
+
+def createDB():
+    try:
+        with psycopg.connect(conn_params, autocommit=True) as conn:
+            with conn.cursor() as cur:
+                new_db_name = 'web_RAG_DB'
+                cur.execute(f"CREATE DATABASE {new_db_name}")
+                logging.info(f"Created new database {new_db_name}")
+    except Exception as e:
+        logging.error(e)
+
+
+
+
+
+
+
+
+
 
